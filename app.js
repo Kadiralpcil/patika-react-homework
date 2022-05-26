0b7fbe3fb8d0f00ba997111b1bfecf485c67ba85
Kadiralpcil <kadiralpcil@gmail.com>
2022-05-26 15:58:30 +0300

    update

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 import axios from "axios"
-// const userData =async (user_id) =>{
 
-//     const {data:userData} = await axios(`https://jsonplaceholder.typicode.com/users/${user_id}`)
-//     console.log(`${user_id}.data:`, userData);
-// }
 export default async function userData(user_id){
     const {data:userData} = await axios(`https://jsonplaceholder.typicode.com/users/${user_id}`)
     const {data:postData} = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`);
-    console.log(`${user_id}.data:`, userData);
-    console.log(`${user_id}.data:`, postData);
+    console.log(userData);
+    console.log(postData[0]);
 };
